70eb0872c8657f8d69e323d23b1b64356b7f23e8
Johnny Gomez <jchengnc@gmail.com>
2016-01-07 22:54:32 -0800

    setup py.test

---
+++ wk1/haystack.py
@@ -0,0 +1,8 @@
+'''It will open and read test data file'''
+'''Find all numerical values and sum them'''
+class Haystack:
+    def __init__(self, value=0):
+        return 0
+
+    def __int__(self):
+        return self._value
