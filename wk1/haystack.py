a8094f18766fefa01ca58ac5d776b39fe57f1cc7
Johnny Gomez <jchengnc@gmail.com>
2016-01-11 22:49:50 -0800

    Finished assignment 8.2 for wk1

--- wk1/haystack.py
+++ wk1/haystack.py
@@ -1,8 +1,44 @@
+import os
+import re
+import pdb
+
+os.chdir('.')
+print(os.getcwd())
 '''It will open and read test data file'''
 '''Find all numerical values and sum them'''
-class Haystack:
-    def __init__(self, value=0):
-        return 0
 
-    def __int__(self):
-        return self._value
+class Haystack(object):
+    def __init__(self, value=''):
+        self.value = value
+        self.numbers = []
+
+    def open_read(self):
+        with open('regex_sum_199880.txt', 'r') as f:
+            self.value  = f.read()
+        f.closed
+
+    """Parse the string and extract the integer"""
+    def parse(self):
+        self.numbers = re.findall(r'\d+', self.value)
+        return self.numbers
+
+    """Sum all the integer"""
+    def sum(self, numbers=[]):
+        results = map(int, numbers)
+        return reduce(lambda a, x: a + x, results)
+
+def main():
+    h = Haystack("I am 17 years old")
+    pdb.set_trace()
+    h.open_read()
+    pdb.set_trace()
+    arr = h.parse()
+    pdb.set_trace()
+    print(arr)
+    s = h.sum(arr)
+    pdb.set_trace()
+    print("sum:", s)
+
+
+if __name__ == '__main__':
+    main()
--- wk1/tests/test_haystack.py
+++ wk1/tests/test_haystack.py
@@ -1,12 +1,11 @@
 import sys, os
-import pytest
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
 from haystack import Haystack
 
 '''Stub some random test and see if the regex can parse just the numbers'''
-def test_haystack():
+def test_parse():
     s = """ Why should you learn to write programs? 7746
             12 1929 8827
             Writing programs (or programming) is a very creative
@@ -15,5 +14,14 @@ def test_haystack():
             8837 a difficult data analysis problem to having fun to helping 128
             someone else solve a problem.  This book assumes that
             everyone needs to know how to program ... """
-    assert True == True
+    hay = Haystack(s)
+    result = hay.parse()
+    assert len(result) == 7
+    assert int(result[6]) == 128
 
+def test_sum():
+    s = "Do not matter"
+    result = ['125', '200', '75']
+    hay = Haystack(s)
+    sum = hay.sum(result)
+    assert int(sum) == 400
